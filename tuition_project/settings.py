96ab2b4db6f3bce5c6e504ce9e2fea9c89fdfddc
mase2020 <masum07@hotmail.co.uk>
2022-03-23 17:06:38 +0000

    secret keyp

--- tuition_project/settings.py
+++ tuition_project/settings.py
@@ -133,12 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = [
- 
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-   
-]
 
 SITE_ID = 1
 
